858c763a6c9ffa265889edccf97d0151cbe3a4ad
Daniil Paskal <pask0850@mylaurier.ca>
2021-10-01 12:56:13 -0400

    Finished part 3

--- app/src/main/java/com/example/androidassignments/ListItemsActivity.java
+++ app/src/main/java/com/example/androidassignments/ListItemsActivity.java
@@ -1,16 +1,137 @@
 package com.example.androidassignments;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.example.androidassignments.R;
 
+import java.io.File;
+
 public class ListItemsActivity extends AppCompatActivity {
+
+    protected static final String ACTIVITY_NAME = "ListItemsActivity";
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    // Declare camera button
+    ImageButton cameraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+
+        // Initialize cameraButton, toastSwitch, checkBox, and radioButton
+        cameraButton = findViewById(R.id.cameraButton);
+        Switch toastSwitch = findViewById(R.id.toastSwitch);
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        RadioButton radioButton = findViewById(R.id.radioButton);
+
+        // When cameraButton is clicked, take picture
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("MediaStore.ACTION_IMAGE_CAPTURE");
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+            }
+        });
+
+        // When switch is toggled, launch toast
+        toastSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Context context = getApplicationContext();
+                CharSequence text;
+                int duration;
+
+                if (b) {
+                    text = context.getString(R.string.toastTrue);
+                    duration = Toast.LENGTH_SHORT;
+                } else {
+                    text = context.getString(R.string.toastFalse);
+                    duration = Toast.LENGTH_LONG;
+                }
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+        // When checkbox is toggled, show dialog box
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListItemsActivity.this);
+                builder.setMessage(R.string.dialogMessage)
+                        .setTitle(R.string.dialogTitle)
+                        .setPositiveButton(R.string.checkboxOk, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent resultIntent = new Intent();
+                                resultIntent.putExtra("Response", getApplicationContext().getString(R.string.checkboxResponse));
+                                setResult(Activity.RESULT_OK, resultIntent);
+                                finish();
+                            }
+                        })
+                        .setNegativeButton(R.string.checkboxCancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                            }
+                        })
+                        .show();
+            }
+        });
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int responseCode, Intent data) {
+        super.onActivityResult(requestCode, responseCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && responseCode == Activity.RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            cameraButton.setImageBitmap(imageBitmap);
+        }
     }
 }
--- app/src/main/java/com/example/androidassignments/MainActivity.java
+++ app/src/main/java/com/example/androidassignments/MainActivity.java
@@ -2,15 +2,75 @@ package com.example.androidassignments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.androidassignments.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected static final String ACTIVITY_NAME = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+
+        Button mainButton = findViewById(R.id.mainButton);
+
+        // When mainButton is clicked, go to ListItemsActivity
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListItemsActivity.class);
+                startActivityForResult(intent, 10);
+            }
+        });
     }
+
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
+
+    protected void onActivityResult(int requestCode, int responseCode, Intent data) {
+        super.onActivityResult(requestCode, responseCode, data);
+        if (requestCode == 10) {
+            Log.i(ACTIVITY_NAME, "Returned to MainActivity.onActivityResult");
+
+            // Check if result code is Activity.RESULT_OK
+            if (responseCode == Activity.RESULT_OK) {
+                String messagePassed = getApplicationContext().getString(R.string.passMessage) + data.getStringExtra("Response");
+                Toast toast = Toast.makeText(MainActivity.this, messagePassed, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+    }
+
 }
